50022518d3d31bb4db7999e17661e78b300fb9de
M Subhan <125981621+subhan-2207e@users.noreply.github.com>
2024-06-13 12:16:37 +0500

    Add files via upload
    Typescript Number Guessing Game

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+console.log("Wellcome to subhan Cli Number Guessing Game");
+const ramdanNumber = Math.floor(Math.random() * 2 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "NumberGuessingGame",
+        type: "number",
+        message: "Enter Guess Number(Number limit 1 to 5):",
+    },
+]);
+if (answer.NumberGuessingGame === ramdanNumber) {
+    console.log("Congratulation ! you guess a correct number");
+}
+else {
+    console.log("Sorry , you guess a wrong number");
+}
---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+console.log("Wellcome to subhan Cli Number Guessing Game")
+
+const  ramdanNumber = Math.floor(Math.random() * 5 +1)
+
+const answer = await inquirer.prompt([
+    {
+        name:"NumberGuessingGame",
+        type:"number",
+        message:"Enter Guess Number(Number limit 1 to 5):",
+    },
+]);
+
+if (answer.NumberGuessingGame === ramdanNumber){
+    console.log("Congratulation ! you guess a correct number")
+}
+else{
+    console.log("Sorry , you guess a wrong number")
+}
+
+
+
+
+
+
